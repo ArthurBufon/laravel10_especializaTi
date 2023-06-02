e011c059bf6c1b1ff6daf5d2e89d1b8b31241ecb
arthur_infus <arthur@infus.inf.br>
2023-06-02 14:39:23 -0300

    api rest atualizando registros

--- app/Http/Requests/StoreUpdateSupport.php
+++ app/Http/Requests/StoreUpdateSupport.php
@@ -27,14 +27,14 @@ class StoreUpdateSupport extends FormRequest
             'body' => 'required|min:3|max:100000'
         ];
 
-        if($this->method() === 'PUT')
+        if($this->method() === 'PUT' || $this->method() === 'PATCH')
         {
             $rules['subject'] = [
                 'required',
                 'min:3',
                 'max:255',
                 // "unique:supports,subject,{$this->id},id",
-                Rule::unique('supports')->ignore($this->id),
+                Rule::unique('supports')->ignore($this->support ?? $this->id),
             ];
         }
 
